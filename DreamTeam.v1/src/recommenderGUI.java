722e7e31b0d50937b4f6a0ccbdd06c305948df15
conorwell <c_wellman@coloradocollege.edu>
2023-12-07 11:48:46 -0700

    empty files sent again

--- DreamTeam.v1/src/recommenderGUI.java
+++ DreamTeam.v1/src/recommenderGUI.java
@@ -1,2 +1,41 @@
-package PACKAGE_NAME;public class recommenderGUI {
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+
+public class recommenderGUI extends JFrame{
+
+    JFrame f = new JFrame();
+    JButton btn = new JButton();
+
+    JTextPane desc = new JTextPane();
+
+    JTextPane resp = new JTextPane();
+    public void init(){
+
+        f.setSize(400,500);
+
+        desc.setText("Ask ChatGPT for a recommendation for your next workout \n" +
+                        "based on your most recent workout!");
+        desc.setEditable(false);
+        f.add(desc);
+
+
+        btn.setSize(50,40);
+        btn.setText("Get Recommendation!");
+        f.add(btn);
+
+
+        resp.setEditable(false);
+        resp.setText("(response here)");
+        resp.setPreferredSize(new Dimension(350,375));
+        f.add(resp);
+
+        f.setVisible(true);
+        f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        f.setLayout(new FlowLayout());
+
+
+    }
+
+
 }

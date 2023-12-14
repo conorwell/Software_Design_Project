9bf45d007a136b7f37bfda3fd2b00f6a28b5e0bf
thehankstah <gmemail3@gmail.com>
2023-12-14 12:02:57 -0700

    Workout and workout model stubs

---
+++ DreamTeam.v1/src/Workout.java
@@ -0,0 +1,8 @@
+public class Workout{
+    public workout Workout(String name, ArrayList<String> exercise, ArrayList<Int> durations, String: comment, String: date, Int: totalDuration){
+
+        //TODO Replace with actual algorithm
+        return workout;
+    }
+}
+
---
+++ DreamTeam.v1/src/WorkoutBuilder.java
@@ -0,0 +1,6 @@
+public class WorkoutBuilder {
+    public workout workoutBuilder(){
+    //TODO Replace with actual algorithm
+        return workout;
+    }
+}

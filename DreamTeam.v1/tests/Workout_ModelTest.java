0a947ab40630420cc99016cd0b0f3aa34ffe3222
conorwell <c_wellman@coloradocollege.edu>
2023-12-19 17:44:50 -0700

    updates to workout_modeltest

--- DreamTeam.v1/tests/Workout_ModelTest.java
+++ DreamTeam.v1/tests/Workout_ModelTest.java
@@ -21,35 +21,22 @@ class Workout_ModelTest {
         durations.add(50);
         durations.add(40);
         Workout_Model model = Workout_Model.getInstance();
-        Workout w1 = new Workout("conorwell","test",exercises,durations,"felt good today",date);
-        Workout w2 = new Workout("zebman","none",exercises,durations,"N/A",date);
-        model.addWorkouts(w1);
-        model.addWorkouts(w2);
-        ArrayList<ArrayList<String>> workouts = model.getWorkouts("conorwell");
-        assertEquals(workouts.get(0).get(0), "conorwell");
-        assertEquals(workouts.get(0).get(1), "test");
-        assertEquals(workouts.get(0).get(2), "felt good today");
-        assertEquals(workouts.get(0).get(3), "100");
-        assertEquals(workouts.get(0).get(4), "10/27/14");
-        assertEquals(workouts.get(0).get(5), "bench");
-        assertEquals(workouts.get(0).get(6), "10");
-        assertEquals(workouts.get(0).get(7), "push");
-        assertEquals(workouts.get(0).get(8), "50");
-        assertEquals(workouts.get(0).get(9), "pull");
-        assertEquals(workouts.get(0).get(10), "40");
+        Workout w1 = new Workout("testcase5","w1",exercises,durations,"felt good today",date);
 
-        ArrayList<ArrayList<String>> workouts2 = model.getWorkouts("zebman");
-        assertEquals(workouts2.get(0).get(0), "zebman");
-        assertEquals(workouts2.get(0).get(1), "none");
-        assertEquals(workouts2.get(0).get(2), "N/A");
-        assertEquals(workouts2.get(0).get(3), "100");
-        assertEquals(workouts2.get(0).get(4), String.valueOf(date));
-        assertEquals(workouts2.get(0).get(5), "bench");
-        assertEquals(workouts2.get(0).get(6), "10");
-        assertEquals(workouts2.get(0).get(7), "push");
-        assertEquals(workouts2.get(0).get(8), "50");
-        assertEquals(workouts2.get(0).get(9), "pull");
-        assertEquals(workouts2.get(0).get(10), "40");
+        model.createWorkoutsList("testcase5");
+        model.addWorkouts(w1);
+
+        ArrayList<ArrayList<String>> workouts = model.getWorkouts("testcase5");
+        assertEquals(workouts.get(0).get(0), "w1");
+        assertEquals(workouts.get(0).get(1), "felt good today");
+        assertEquals(workouts.get(0).get(2), "100");
+        assertEquals(workouts.get(0).get(3), "10/27/14");
+        assertEquals(workouts.get(0).get(4), "bench");
+        assertEquals(workouts.get(0).get(5), "10");
+        assertEquals(workouts.get(0).get(6), "push");
+        assertEquals(workouts.get(0).get(7), "50");
+        assertEquals(workouts.get(0).get(8), "pull");
+        assertEquals(workouts.get(0).get(9), "40");
     }
 
     @Test
@@ -70,24 +57,23 @@ class Workout_ModelTest {
         durations.add(10);
         durations.add(50);
         durations.add(40);
-        Workout w = new Workout("conorwell","test",exercises,durations,"felt good today",date);
+        Workout w = new Workout("editTester1","w",exercises,durations,"felt good today",date);
         Workout_Model model = Workout_Model.getInstance();
+        model.createWorkoutsList("editTester1");
         model.addWorkouts(w);
-        Workout wEdit = new Workout("conorwell","testing",exercises,durations,"felt bad today", date);
+        Workout wEdit = new Workout("editTester1","wEdit",exercises,durations,"felt bad today", date);
         model.editWorkout(wEdit);
 
-        ArrayList<ArrayList<String>> workouts = model.getWorkouts("conorwell");
-        assertEquals(workouts.get(0).get(0), "conorwell");
-        assertEquals(workouts.get(0).get(1), "testing");
-        assertEquals(workouts.get(0).get(2), "felt bad today");
-        assertEquals(workouts.get(0).get(3), "100");
-        assertEquals(workouts.get(0).get(4), "10/27/20");
-        assertEquals(workouts.get(0).get(5), "bench");
-        assertEquals(workouts.get(0).get(6), "10");
-        assertEquals(workouts.get(0).get(7), "push");
-        assertEquals(workouts.get(0).get(8), "50");
-        assertEquals(workouts.get(0).get(9), "pull");
-        assertEquals(workouts.get(0).get(10), "40");
+        ArrayList<ArrayList<String>> workouts = model.getWorkouts("editTester1");
+        assertEquals(workouts.get(0).get(1), "felt bad today");
+        assertEquals(workouts.get(0).get(2), "100");
+        assertEquals(workouts.get(0).get(3), "10/27/20");
+        assertEquals(workouts.get(0).get(4), "bench");
+        assertEquals(workouts.get(0).get(5), "10");
+        assertEquals(workouts.get(0).get(6), "push");
+        assertEquals(workouts.get(0).get(7), "50");
+        assertEquals(workouts.get(0).get(8), "pull");
+        assertEquals(workouts.get(0).get(9), "40");
     }
 
 
